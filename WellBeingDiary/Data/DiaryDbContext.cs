0e0b38cfb8580702fc493a04f058c9d9be74dbe3
Filip Furgał <ffurga@gmail.com>
2024-04-27 23:21:28 +0200

    Created connection and initialized Database

---
+++ WellBeingDiary/Data/DiaryDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using WellBeingDiary.Entities;
+
+namespace WellBeingDiary.Data
+{
+    public class DiaryDbContext : DbContext
+    {
+        public DiaryDbContext(DbContextOptions<DiaryDbContext> options) : base(options){ }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<DiaryNote> DiaryNotes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
