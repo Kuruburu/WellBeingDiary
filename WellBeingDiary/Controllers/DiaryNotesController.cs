b426d0701f0ccb46520bfd53fcb5fd8dfee411a5
Filip Furgał <ffurga@gmail.com>
2024-05-10 21:57:42 +0200

    Added tokens to swagger, and made GetAll diary notes accessible only to admin

--- WellBeingDiary/Controllers/DiaryNotesController.cs
+++ WellBeingDiary/Controllers/DiaryNotesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -25,6 +26,7 @@ namespace WellBeingDiary.Controllers
         }
 
         [HttpGet]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> GetAll()
         {
             var diaryNotes = await _diaryRepo.GetAllAsync();
